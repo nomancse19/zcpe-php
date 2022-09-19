e93b147ebc96b36511f9a9d1a4969ab36af1397e
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-19 22:55:59 +0600

    Update manual_array_filter.php

--- function/lambda-anonymous/manual_array_filter.php
+++ function/lambda-anonymous/manual_array_filter.php
@@ -2,12 +2,19 @@
 
 $a=range(1,100);
 
-function manual_array_filter(array $ar_data,$callback_function){
-    return $callback_function($ar_data);
+function manual_array_filter(array $ar_data,callable $callback_function){
+    $final_array=[];
+    foreach($ar_data as $value){
+        if($callback_function($value)){
+            $final_array[]=$value;
+        }
+
+    }
+    return $final_array;
 }
 
 $check= manual_array_filter($a,function($number){
-    return $number%2==0;
+    return $number%2==1;
 });
 echo "<pre>";
 print_r($check);
